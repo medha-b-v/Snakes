b0a417f6cb6dd133be6fa234e18e9cc46f651f11
medha-b-v <medhabv703@gmail.com>
2024-06-30 00:04:24 +0530

    Making a basic snakes game with vanilla js

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener(DOMContentLoaded, () => {
+    const grid= document.querySelector('.grid');
+    let squares= Array.from(document.querySelector('.grid div'));
+    
+
+
+
+
+})
