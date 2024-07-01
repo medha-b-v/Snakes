ef88b3a7c49963322a8868428cd4653a07f49f9b
medha-b-v <medhabv703@gmail.com>
2024-07-01 14:26:35 +0530

    linked list of snake done

--- app.js
+++ app.js
@@ -1,9 +1,60 @@
-document.addEventListener(DOMContentLoaded, () => {
-    const grid= document.querySelector('.grid');
-    let squares= Array.from(document.querySelector('.grid div'));
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    let ScoreDisplay = document.querySelector('#score');
+    const StartButton = document.querySelector('.start-button');
     
+    class Node {
+        constructor(position) {
+            this.position = position;
+            this.next = null;
+        }
+    }
+    
+    class LinkedList {
+        constructor() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        append(position) {
+            const newNode = new Node(position);
+            if (!this.head) {
+                this.head = newNode;
+                this.tail = newNode;
+            } else {
+                this.tail.next = newNode;
+                this.tail = newNode;
+            }
+        }
+    }
+
+    let snake = new LinkedList();
 
 
+    function snakeincrease() {
+        let snakeElements = document.getElementsByClassName('snake');
+        Array.from(snakeElements).forEach(element => {
+            let index = squares.indexOf(element); 
+            if (index !== -1) {
+                snake.append(index); 
+            }
+        });
+    }
 
+    const width = 10;
+    let currentPosition = 15;
 
-})
+    function draw() {
+        let current = snake.head;
+        while (current) {
+            squares[current.position].classList.add('snake');
+            squares[current.position].style.backgroundColor = 'black';
+            current = current.next; 
+        }
+    }
+
+    snakeincrease();
+    draw(); 
+    console.log(snake.head);
+});
